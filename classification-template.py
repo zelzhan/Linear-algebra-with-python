7cf1810cd3d2b7b6a892a6f73626121674886e3d
zelzhan <elzhan.zeinulla@nu.edu.kz>
2018-05-22 02:28:15 +0600

    modified classification template

---
+++ classification-template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env conda
+# -*- coding: utf-8 -*-
+"""
+* ****************************************************************************
+*      Owner: stayal0ne <elzhan.zeinulla@nu.edu.kz>                          *
+*      Github: https://github.com/zelzhan                                    *
+*      Created: Tue May 22 01:14:42 2018 by stayal0ne                        *
+******************************************************************************
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+
+# importing dataset
+dataset = pd.read_csv('dataset.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# splitting the dataset into the training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+
+# feature scaling
+scale = StandardScaler()
+X_train = scale.fit_transform(X_train)
+X_test = scale.transform(X_test)
+
+# Fitting classifier into the training set
+classifier = 0
+'''create your classifier right over here'''
+
+# Prediction
+y_predicted = classifier.predict(X_test)
+
+# Checking the accuracy
+con_matrix = confusion_matrix(y_test, y_predicted)
+
+# visualisation
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arrange(start = X_set[:, 0].min() - 1,
+                                stop = X_set[:, 0].max() + 1, step = 0.01), np.arrange(
+                                        start = X_set[:, 1].min() - 1,
+                                        stop = X_set[:, 1].max() + 1, step = 0.01))
+
+
+
