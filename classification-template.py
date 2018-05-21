feb1e82b7ce460e4bf50e5e446709adab29803f1
zelzhan <elzhan.zeinulla@nu.edu.kz>
2018-05-22 02:30:11 +0600

    classification template 2.0

--- classification-template.py
+++ classification-template.py
@@ -40,12 +40,47 @@ y_predicted = classifier.predict(X_test)
 # Checking the accuracy
 con_matrix = confusion_matrix(y_test, y_predicted)
 
-# visualisation
+# visualisation of Training set
 X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arrange(start = X_set[:, 0].min() - 1,
-                                stop = X_set[:, 0].max() + 1, step = 0.01), np.arrange(
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                                stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(
                                         start = X_set[:, 1].min() - 1,
                                         stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.countourf(X1, X2, classifier.predict(
+        np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.xlim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title("Classification set")
+plt.xlabel("X-axis")
+plt.ylabel("y-axis")
+plt.legend
+plt.show()
+
+# visualisation of Test set
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                                stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(
+                                        start = X_set[:, 1].min() - 1,
+                                        stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.countourf(X1, X2, classifier.predict(
+        np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.xlim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title("Classification set")
+plt.xlabel("X-axis")
+plt.ylabel("y-axis")
+plt.legend
+plt.show()
 
 
 
