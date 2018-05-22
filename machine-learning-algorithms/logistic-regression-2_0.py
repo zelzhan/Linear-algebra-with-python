a985227caf8e1aeb88d94fc65ba90f77cb83bf09
zelzhan <elzhan.zeinulla@nu.edu.kz>
2018-05-22 06:44:48 +0600

    one more fail. Confusion matrix shows wrong predictions

--- machine-learning-algorithms/logistic-regression-2_0.py
+++ machine-learning-algorithms/logistic-regression-2_0.py
@@ -4,41 +4,48 @@
 * ****************************************************************************
 *      Owner: stayal0ne <elzhan.zeinulla@nu.edu.kz>                          *
 *      Github: https://github.com/zelzhan                                    *
-*      Created: Tue May 22 03:49:23 2018 by stayal0ne                                        *
+*      Created: Tue May 22 05:06:23 2018 by stayal0ne                                        *
 ******************************************************************************
 """
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
 from matplotlib.colors import ListedColormap
+from sklearn.preprocessing import Imputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 # importing dataset
-dataset = pd.read_csv('/home/stayal0ne/Machine-learning/datasets/bank.csv', sep=';')
-dataset['age'] = dataset['age'].astype(float)
-dataset['balance'] = dataset['balance'].astype(float)
-X = dataset.iloc[:, [0, 5]].values
+dataset = pd.read_csv('/home/stayal0ne/Machine-learning/datasets/banking.csv')
+mapping = {"no": 0, "yes": 1, "unknown": None}             #dealing with missing categorical data
+dataset['loan'] = dataset['loan'].map(mapping)
+X = dataset.iloc[:, [1, 2, 6, 11]].values
 y = dataset.iloc[:, -1].values
 
+# fill in missing values
+imp = Imputer(missing_values="NaN", strategy="most_frequent", axis=0)
+imp = imp.fit(X[:, [2]])
+X[:, [2]] = imp.transform(X[:, [2]])
 
+# encoding categorial data
+label_encoder_X = LabelEncoder()
+X[:, 1] = label_encoder_X.fit_transform(X[:, 1])       
+X[:, 0] = label_encoder_X.fit_transform(X[:, 0])             #fit and transform 1st column of the dataset
+one_hot_encoder = OneHotEncoder(categorical_features=[0, 1])          #create an OneHotEncoder object specifying the column
+X = one_hot_encoder.fit_transform(X).toarray()                        #OneHot encode
+df = pd.DataFrame(X) 
 
 # splitting the dataset into the training and test sets
 X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.25, random_state=42)
-
-# encoding categorial data
-label_encoder_y = LabelEncoder()
-y = label_encoder_y.fit_transform(y)
+        X, y, test_size=0.2, random_state=42)
 
 # feature scaling
 scale = StandardScaler()
-X_train = scale.fit_transform(X_train)
-X_test = scale.transform(X_test)
+X_train[:, [16, 17]] = scale.fit_transform(X_train[:, [16, 17]])
+X_test[:, [16, 17]] = scale.transform(X_test[:, [16, 17]])
 
 # Fitting classifier into the training set
 classifier = LogisticRegression(random_state=42)
@@ -49,7 +56,6 @@ y_predicted = classifier.predict(X_test)
 
 # Checking the accuracy
 con_matrix = confusion_matrix(y_test, y_predicted)
-
 
 # visualisation of Training set
 X_set, y_set = X_test, y_test
@@ -79,7 +85,7 @@ X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
                                         start = X_set[:, 1].min() - 1,
                                         stop = X_set[:, 1].max() + 1, step = 0.01))
 
-plt.contourf(X1, X2, classifier.predict(
+plt.countourf(X1, X2, classifier.predict(
         np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
